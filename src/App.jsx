ba8740e2a7a8bc090ccc649c457b908a146da28e
Hamna Ishaq <hamnaishaq1@gmail.com>
2023-07-05 22:33:44 +0500

    Check if desired photo was clicked in the where's waldo style image

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,40 @@
+import "./assets/style.css";
+
 const App = () => {
-  return <>testing</>;
+  const onClickCheckImage = (e) => {
+    // min and max percentage values of image to found
+    const minX = 45;
+    const minY = 12;
+
+    const maxX = 47;
+    const maxY = 19;
+
+    // clicked coord in percentage
+    const X = Math.floor((e.clientX / window.innerWidth) * 100);
+    const Y = Math.floor((e.clientY / window.innerHeight) * 100);
+
+    console.log("(", X, Y, ")");
+
+    // check if the image was clicked
+    if (X >= minX && Y >= minY && X <= maxX && Y <= maxY) {
+      console.log("image found");
+    } else {
+      console.log("keep looking");
+    }
+  };
+
+  return (
+    <>
+      <main>
+        <img
+          className="waldo-pic"
+          src="/wheres_waldo.jpg"
+          alt="where is waldo style image"
+          onClick={onClickCheckImage}
+        />
+      </main>
+    </>
+  );
 };
 
 export default App;
